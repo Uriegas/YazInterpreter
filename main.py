5f2e3ae1ebec75eb441d1a2d6c67a757c34d9da3
Uriegas <1930526@upv.edu.mx>
2021-01-26 17:56:40 -0600

    Interprete funciona REPEAT no acepta espacios en blanco

--- main.py
+++ main.py
@@ -2,14 +2,7 @@
 #Lexer --> String 2 Tokens
 #Evaluate --> Tokens 2 Resultado
 
-class Token:
-    def __init__(self, a, b):
-        self.id = a
-        self.value = b
-    def printToken(self):
-        print(self.id, self.value)
-
-class Lexer():
+class Interpreter():
     def parse(self, str):
         self.string = str
         self.splited = string.split()
@@ -17,7 +10,6 @@ class Lexer():
         print(self.string, self.splited)
     def evalute(self):
         if(self.splited[0] == "CONVERT"):
-            print("Its a convert")
             if(self.splited[2] == "F"):
                 print( 1.8 * float(self.splited[1]) + 32 )
             elif(self.splited[2] == "C"):
@@ -26,18 +18,17 @@ class Lexer():
             for num in range(int(self.splited[1]), int(self.splited[2]), int(self.splited[3])):
                 print(num, end = " ")
             print()
-            print("Its a Range")
         elif(self.splited[0] == "REPEAT"):
-            for rep in range(int(self.splited[2]), len(self.splited), 2):
+            for rep in range(2, len(self.splited), 2):
+#                print(rep, self.splited[rep])
                 print(self.splited[rep - 1].replace('"', '').replace('_', ' ') * int(self.splited[rep]), end = '')
             print()
-            print("Its a repeat")
         elif(self.splited[0] == "END"):
             exit(self)
 
-mylexer = Lexer()
+interpreter = Interpreter()
 while True:
     string = input(">")
-    mylexer.parse(string)
-    mylexer.print()
-    mylexer.evalute()
+    interpreter.parse(string)
+    interpreter.print()
+    interpreter.evalute()
