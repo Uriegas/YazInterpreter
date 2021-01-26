bb24a954f959843ee3f1b7f96ba664231a9abac0
Uriegas <1930526@upv.edu.mx>
2021-01-25 21:29:24 -0600

    Clase Lexer

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Proyecto Universidad Politécnica de Victora
 Profesor: Marco Aurelio Nuño Maganda  
 Materia: Fundamentos de Programación Orientada a Objetos  
 Basado en: [YazInterpreter](https://courses.cs.washington.edu/courses/cse142/20wi/homework/6/assign6.pdf)  
-Reporte: [Overleaf](https://www.overleaf.com/read/ybxgbvpctzbg)
+Reporte: [Ver](https://www.overleaf.com/read/ybxgbvpctzbg) [Editar](https://www.overleaf.com/3137935175qcthrzcqdjsc)
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-class token:
+#Class YazInterpreter
+#Lexer --> String 2 Tokens
+#Evaluate --> Tokens 2 Resultado
+
+class Token:
     def __init__(self, a, b):
         self.id = a
         self.value = b
     def printToken(self):
         print(self.id, self.value)
 
-mytoken = token(10, "RANGE")
-print(token)
-mytoken.printToken()
+class Lexer:
+    def __init__(self):
+        self.string
+        self.splited
+    def parse(self, str):
+        self.string = str
+        self.splited = self.string.splited()
+
+mylexer = Lexer()
+while True:
+    string = input(">")
+    mylexer.parse(string)
