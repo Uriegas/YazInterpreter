afde2b6f5c6cc9c50c377c6dfb1df01a73ac048d
Uriegas <1930526@upv.edu.mx>
2021-01-25 21:51:18 -0600

    Comando CONVERT funcionando

--- main.py
+++ main.py
@@ -9,15 +9,29 @@ class Token:
     def printToken(self):
         print(self.id, self.value)
 
-class Lexer:
-    def __init__(self):
-        self.string
-        self.splited
+class Lexer():
     def parse(self, str):
         self.string = str
-        self.splited = self.string.splited()
+        self.splited = string.split()
+    def print(self):
+        print(self.string, self.splited)
+    def evalute(self):
+        if(self.splited[0] == "CONVERT"):
+            print("Its a range")
+            if(self.splited[2] == "F"):
+                print( 1.8 * float(self.splited[1]) + 32 )
+            elif(self.splited[2] == "C"):
+                print( (float(self.splited[1]) - 32) / 1.8 )
+        elif(self.splited[0] == "RANGE"):
+            print("Its a convert")
+        elif(self.splited[0] == "REPEAT"):
+            print("Its a repeat")
+        elif(self.splited[0] == "END"):
+            return 0
 
 mylexer = Lexer()
 while True:
     string = input(">")
     mylexer.parse(string)
+    mylexer.print()
+    mylexer.evalute()
