b78448f40b20f8c353ea55c8a9fcc4a0c69ed09b
Uriegas <1930526@upv.edu.mx>
2021-01-26 18:12:00 -0600

    Documentacion Clase Interpreter

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 #Class YazInterpreter
-#Lexer --> String 2 Tokens
-#Evaluate --> Tokens 2 Resultado
 
+##Clase del Interprete: Acepta un comando 'x' con arguementos...
 class Interpreter():
-    def parse(self, str):
+    ##Almacena un string en el interprete y hace una lista de esa string
+    def getline(self, str):
         self.string = str
         self.splited = string.split()
+
+    ##Para imprimir que tiene dentro la clase
     def print(self):
         print(self.string, self.splited)
+
+    ##Da el resultado del comando utilizado
     def evalute(self):
         if(self.splited[0] == "CONVERT"):
             if(self.splited[2] == "F"):
@@ -20,15 +24,28 @@ class Interpreter():
             print()
         elif(self.splited[0] == "REPEAT"):
             for rep in range(2, len(self.splited), 2):
-#                print(rep, self.splited[rep])
                 print(self.splited[rep - 1].replace('"', '').replace('_', ' ') * int(self.splited[rep]), end = '')
             print()
         elif(self.splited[0] == "END"):
-            exit(self)
+            exit(self) ##Aqui debe de regresar al menu
+    
+    ##Recibe un string y devuelve el resultado del comando (Hace las 2 funciones de arriba juntas)
+    def evaluateString(self, str):
+        self.getline(str)
+        self.evalute()
 
+
+##Este es como el main()
+##Crea el interprete de la clase interprete
 interpreter = Interpreter()
+
+##Ciclo infinito para leer, esto debe de ir en la clase Menu
 while True:
     string = input(">")
-    interpreter.parse(string)
-    interpreter.print()
-    interpreter.evalute()
+    interpreter.evaluateString(string)
+
+##Funcionamiento mas detallado (se puede ocupar)
+##    interpreter.getline(string)
+##    interpreter.print()
+##    interpreter.evalute()
+    
